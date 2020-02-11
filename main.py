6004660ab90d34d0710e1eee825d43b0b3eabf02
Victoria <vmoix001@mail.ru>
2020-02-11 23:27:10 +0700

    to do

---
+++ main.py
@@ -0,0 +1,60 @@
+# Case-study 1
+# Developers: Moiseenko V.( %)
+#             Vlasov V.( %)
+# This programm allows to draw different fractal pictures
+
+import turtle
+
+# For both: draw the binary tree
+def binary_tree():
+    pass
+
+# For Victoria: draw the Koch curve
+def koch_curve():
+    pass
+
+# For Vlas: draw the Koch snowflake
+def koch_snowflake():
+    pass
+
+# For Victoria: draw the ice fractal (example 1)
+def ice_fractal1():
+    pass
+
+# For Vlas: draw the ice fractal (example 2)
+def ice_fractal2():
+    pass
+
+# For Victoria: draw the snowflake for the first ice fractal
+def snowflake_ice1():
+    pass
+
+# For Vlas: draw the snowflake for the second ice fractal
+def snowflake_ice2():
+    pass
+
+# For Victoria: draw the Minkovsky curve
+def minkovsky_curve():
+    pass
+
+# For Vlas: draw the Levi curve
+def levi_curve():
+    pass
+
+def main():
+    binary_tree()
+    koch_curve()
+    koch_snowflake()
+    ice_fractal1()
+    ice_fractal2()
+    snowflake_ice1()
+    snowflake_ice2()
+    minkovsky_curve()
+    levi_curve()
+
+main()
+
+turtle.done()
+
+
+
